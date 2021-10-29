5f6282ff33c61b6dd783c6b44efc0768dd19cfc4
MK-Raihan <mkraihan207@gmail.com>
2021-10-29 10:26:46 +0600

    added banner

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,14 +7,14 @@ const Header = () => {
         color: "red"
       }
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
         <div className="container">
             <a className="navbar-brand fs-2 fw-bold text-primary" href="/"> <span><i class="fas fa-trailer"></i></span> Mind Fress Traveling</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
                 <li className="nav-item">
                     <NavLink activeStyle={activeStyle} className="nav-link " aria-current="page" to="/">Home</NavLink>
                 </li>
